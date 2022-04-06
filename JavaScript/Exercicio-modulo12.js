bd91a06fd1c86b6dec042ba83283f41b82f5fe1e
Eric <ericferrarir1@gmail.com>
2022-04-06 15:42:35 -0300

    adicionando media 0.2

--- JavaScript/Exercicio-modulo12.js
+++ JavaScript/Exercicio-modulo12.js
@@ -1,11 +1,13 @@
 function obterMedia () {
 
-    var a = document.getElementById("a").value;
-    var b = document.getElementById("b").value;
-    var c = document.getElementById("c").value;
+    var a = parseint(document.getElementById("a").value);
+    var b = parseint(document.getElementById("b").value);
+    var c = parseint(document.getElementById("c").value);
 
     var media = (a + b + c) / 3;
 
     document.getElementById("resultado").innerHTML = "Resultado:" + media
+
+    console.log(a + b + c);
 
 }
