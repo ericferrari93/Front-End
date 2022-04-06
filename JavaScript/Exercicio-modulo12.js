189e38f6f1bcb800a73968daaa191b2d961de110
Eric <ericferrarir1@gmail.com>
2022-04-06 15:47:53 -0300

    0.2.1

--- JavaScript/Exercicio-modulo12.js
+++ JavaScript/Exercicio-modulo12.js
@@ -1,8 +1,8 @@
 function obterMedia () {
 
-    var a = parseint(document.getElementById("a").value);
-    var b = parseint(document.getElementById("b").value);
-    var c = parseint(document.getElementById("c").value);
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("b").value);
+    var c = parseInt(document.getElementById("c").value);
 
     var media = (a + b + c) / 3;
 
